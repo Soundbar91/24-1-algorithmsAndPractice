b085959d1f33c0841e99cb97746f519221a46b83
신관규 <soundbar91@gmail.com>
2024-03-18 22:25:19 +0900

    2024.03.18.

--- src/chap2/A.java
+++ src/chap2/A.java
@@ -6,24 +6,24 @@ import java.util.StringTokenizer;
 public class A {
     static int H;
     static int W;
+    static int result;
     static char[][] map;
     static int[][] dx = {
-            {0, 1, 0},
-            {0, 1, 1},
+            {0, 0, -1},
             {0, 0, 1},
-            {0, 1, 1}
+            {0, 1, 1},
+            {0, 1, 0}
     };
 
     static int[][] dy = {
-            {0, 0, 1},
-            {0, 0, 1},
             {0, 1, 1},
+            {0, 1, 1},
+            {0, 0, 1},
             {0, 0, 1}
     };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
@@ -32,7 +32,8 @@ public class A {
             StringTokenizer st = new StringTokenizer(br.readLine());
             H = Integer.parseInt(st.nextToken());
             W = Integer.parseInt(st.nextToken());
-            char[][] map = new char[H][W];
+            map = new char[H][W];
+            result = 0;
             int cnt = 0;
 
             for (int i = 0; i < H; i++) {
@@ -45,36 +46,57 @@ public class A {
 
             if (cnt % 3 != 0) {
                 sb.append(0).append('\n');
-                continue;
             }
             else {
-                for (int i = 0; i < H; i++) {
-                    for (int j = 0; j < W; j++) {
-                        if (map[i][j] == '.') solve(i, j, 0, cnt);
+                solve(cnt);
+                sb.append(result).append('\n');
+            }
+        }
+
+        System.out.print(sb);
+    }
+
+    public static void solve(int cnt) {
+        if (cnt == 0) {
+            result++;
+        }
+        else {
+            for (int i = 0; i < H; i++) {
+                for (int j = 0; j < W; j++) {
+                    if (map[i][j] == '.') {
+                        for (int k = 0; k < 4; k++) {
+                            if (check(j, i, k)) {
+                                flip(j, i, k, '#');
+                                solve(cnt - 3);
+                                flip(j, i, k, '.');
+                            }
+                        }
                     }
                 }
             }
         }
     }
 
-    public static void solve(int x, int y, int depth, int cnt) {
-        if (cnt / 3 == depth) {
+    public static boolean check(int x, int y, int n) {
+        for (int i = 0; i < 3; i++) {
+            int ux = x + dx[n][i];
+            int uy = y + dy[n][i];
 
+            if (!valid(ux, uy) || map[uy][ux] == '#') return false;
         }
-        else {
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 3; j++) {
-                    int ux = x + dx[i][j];
-                    int uy = y + dx[i][j];
-
-                    if (valid(ux, uy) || map[uy][ux] == '#') continue;
-                }
-
-            }
-        }
+        return true;
     }
 
     public static boolean valid(int x, int y) {
         return (x >= 0 && x < W) && (y >= 0 && y < H);
+    }
+
+    public static void flip(int x, int y, int n, char ch) {
+        for (int i = 0; i < 3; i++) {
+            int ux = x + dx[n][i];
+            int uy = y + dy[n][i];
+
+            map[uy][ux] = ch;
+        }
     }
 }
