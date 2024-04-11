7adedfeae8296e69012b1c7c0a29b51c67f496b7
신관규 <soundbar91@gmail.com>
2024-04-11 13:08:39 +0900

    2024.04.11.

---
+++ src/chap6/C.java
@@ -0,0 +1,13 @@
+package chap6;
+
+import java.io.*;
+
+public class C {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        
+    }
+}
