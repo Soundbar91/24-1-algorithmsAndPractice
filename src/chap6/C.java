dc1e4e6cc79cff46fb174f945d259db9d091ab9d
신관규 <soundbar91@gmail.com>
2024-04-13 15:23:34 +0900

    2024.04.13.

--- src/chap6/C.java
+++ src/chap6/C.java
@@ -1,13 +1,99 @@
 package chap6;
 
 import java.io.*;
+import java.util.StringTokenizer;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class C {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            int[] numArr = new int[N];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) numArr[i] = Integer.parseInt(st.nextToken());
+
+            solve(numArr);
+        }
+
+        br.close();
+    }
+
+    public static void solve(int[] A) throws IOException {
+        int median = Rselect(A, A.length / 2);
+
+        int[] result = new int[A.length];
+        int lo = 0, hi = A.length - 1;
+
+        for (int i : A) {
+            if (i < median) result[lo++] = i;
+            else if (i > median) result[hi--] = i;
+        }
+
+        for (int i = lo; i <= hi; i++) result[i] = median;
+
+        lo = (A.length + 1) / 2;
+        hi = A.length;
+
+        for (int i = 0; i < A.length; i++) {
+            A[i] = (i % 2 == 0) ? result[--lo] : result[--hi];
+        }
+
+        printArray(A);
+    }
+
+    public static int Rselect(int[] A, int i) {
+        return RSelect(A, 0, A.length - 1, i);
+    }
+
+    public static int RSelect(int[] A, int lo, int hi, int i) {
+        if (lo == hi) return A[lo];
+
+        int pivotLoc = chooseRandomPivot(lo, hi);
+        swap(A, lo, pivotLoc);
+        pivotLoc = partition(A, lo, hi);
+
+        if (i == pivotLoc) return A[pivotLoc];
+        else if (i < pivotLoc) return RSelect(A, lo, pivotLoc - 1, i);
+        else return RSelect(A, pivotLoc + 1, hi, i);
+    }
+
+    public static int chooseRandomPivot(int lo, int hi) {
+        return ThreadLocalRandom.current().nextInt(lo, hi + 1);
+    }
+
+    public static int partition(int[] A, int lo, int hi) {
+        int pivot = A[lo];
+        int L = lo + 1;
+        int R = hi;
+
+        while(L <= R) {
+            while(L <= R && A[L] <= pivot) ++L;
+            while(L <= R && A[R] > pivot) --R;
+            if (L < R) swap(A, L++, R--);
+        }
+
+        swap(A, lo, R);
+        return R;
+    }
+
+    public static void swap(int[] A, int L, int R) {
+        int temp = A[L];
+        A[L] = A[R];
+        A[R] = temp;
+    }
+
+    public static void printArray(int[] A) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        
+        sb.append(A.length).append('\n');
+        for (int i : A) sb.append(i).append(' ');
+        bw.write(sb + "\n");
+        bw.flush();
     }
 }
